eefce07bac7ef6d737f916d75744e2098f4568f4
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-09-13 17:05:42 +0200

    continue with class definition

--- Agents/SentinelDefense.py
+++ Agents/SentinelDefense.py
@@ -1,21 +1,24 @@
 
-from pysc2.agents import base_agents
+from pysc2.lib import features
 from pysc2.lib import actions
 
-Class Sentry():
+
+
+Class Sentry(sentry, n_sentrys, n_enemies):
   '''Defines how the sentry SC2 unit works'''
   
-  def Force_Field():
+  
+  def Force_Field(sentry):
     '''Function related with Force Field creation'''
     _FORCE_FIELD = actions.FUNCTIONS.Effect_ForceField_screen.id
 
     
-  def Guardian_Shield():
+  def Guardian_Shield(sentry):
     '''Function related with Shield creation'''
     _GUARD_FIELD = actions.FUNCTIONS.Effect_GuardianShield_quick.id
-   
+    return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
   
-  def Hallucinations():
+  def Hallucinations(sentry):
     '''Functions related with Hallucination'''
     
     _HAL_ADEPT = actions.FUNCTIONS.Hallucination_Adept_quick.id
@@ -28,11 +31,11 @@ Class Sentry():
     _HAL_STALKER = actions.FUNCTIONS.Hallucination_Stalker_quick.id
     _HAL_VOIDRAID = actions.FUNCTIONS.Hallucination_VoidRay_quick.id
     _HAL_ZEALOT = actions.FUNCTIONS.Hallucination_Zealot_quick.id
-
+    return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
     
-  def Standard_Functions():
+  def Standard_Functions(sentry):
     '''Standard Functions related with movements and exploration '''
   _NOOP = actions.FUNCTIONS.no_op.id
   _SELECT_POINT = actions.FUNCTIONS.select_point.id
-
+  return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
 
