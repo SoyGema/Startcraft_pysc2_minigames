ced5aac5a22fd9a6f03a24cf2872557eb4b99a89
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-10-18 19:22:36 +0200

    Update SentryDefense.py

--- Agents/SentryDefense.py
+++ Agents/SentryDefense.py
@@ -8,14 +8,6 @@ from pysc2.agents import base_agent
 from pysc2.lib import features
 from pysc2.lib import actions
 
-_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
-_PLAYER_NEUTRAL = 3
-_PLAYER_HOSTILE = 4 
-_NO_OP = actions.FUNCTIONS.Move_screen.id
-_ATTACK_SCREEN = actions.FUNCTIONS.Attack_screen.id
-_SELECT_ARMY = actions.FUNCTIONS.select_army.id
-_NOT_QUEUED = [0]
-_SELECT_ALL = [0]
 
 class Sentry():
   '''Defines how the sentry SC2 unit works'''
@@ -51,34 +43,4 @@ class Sentry():
   _SELECT_POINT = actions.FUNCTIONS.select_point.id
   return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
 
-class SentryDefense(base_agent.BaseAgent):
-  '''An agent specifically for solving the SentryDefense map.'''
-  def step(self,obs):
-    super(SentryDefense, self).step(obs)
-    if _ATTACK_SCREEN in obs.observation["available_actions"]:
-      player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-      sentry_y, sentry_x = (player_relative == _PLAYER_HOSTILE).nonzero()
- 
-      
-      #Build a loop that makes the sentry do a guardian shield
-      if sentry_y.any():
-        return Guardian_Shield(sentry)
-      
-      #Build a loop that makes the sentry to do force field 
-      #measure if the distance is near to enemy?
-      if sentry_y.any():
-        return Force_Field(sentry)
-      
-      if sentry_y.any() and stalker_y.any():
-        return actions.FunctionCall(_NO_OP, [])
-      index = numpy.argmax(sentry_y)
-      index2 = numpy.argmax(stalker_y)
-      target = [sentry_x[index], sentry_y[index]]
-      return actions.FunctionCall(_ATTACK_SCREEN, [_NOT_QUEUED, target]) 
-    elif _SELECT_ARMY in obs.observation["available_actions"]:
-      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
-    else:
-      return actions.FunctionCall(_NO_OP, [])
-    
-                              
-      
+
