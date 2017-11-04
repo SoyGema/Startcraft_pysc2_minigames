f320ea6da17bdedfdcb57e387a77dc534d812a13
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-11-04 16:48:40 -0700

    Update SentryDefense.py

--- Agents/SentryDefense.py
+++ Agents/SentryDefense.py
@@ -8,6 +8,37 @@ from pysc2.agents import base_agent
 from pysc2.lib import features
 from pysc2.lib import actions
 
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_PLAYER_FRIENDLY = 1
+_PLAYER_NEUTRAL = 3  # beacon/minerals
+_PLAYER_HOSTILE = 4
+_NO_OP = actions.FUNCTIONS.no_op.id
+_MOVE_SCREEN = actions.FUNCTIONS.Move_screen.id
+_ATTACK_SCREEN = actions.FUNCTIONS.Attack_screen.id
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_NOT_QUEUED = [0]
+_SELECT_ALL = [0]
+_FORCE_FIELD = actions.FUNCTIONS.Effect_ForceField_screen.id
+
+class SentryForceField(base_agent.BaseAgent):
+  """An agent specifically for solving the ForceField map."""
+
+  def step(self, obs):
+    super(SentryForceField, self).step(obs)
+    if _FORCE_FIELD in obs.observation["available_actions"]:
+      player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
+      hydralisk_y, hydralisk_x = (player_relative == _PLAYER_HOSTILE).nonzero()
+      if not hydralisk_y.any():
+        return actions.FunctionCall(_NO_OP, [])
+      index = numpy.argmax(hydralisk_y)
+      target = [hydralisk_x[index], hydralisk_y[index]]
+      return actions.FunctionCall(_FORCE_FIELD, [_NOT_QUEUED, target])
+    elif _SELECT_ARMY in obs.observation["available_actions"]:
+      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
+    else:
+      return actions.FunctionCall(_NO_OP, [])
+  
+
 
 class Sentry():
   '''Defines how the sentry SC2 unit works'''
