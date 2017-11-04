d6ef9449f08f3b4a5227a6a83569ff49abe99f3e
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-11-03 18:57:00 -0700

    Update Hallucination scripted agent

--- new_minigames/SentryForceField/scripted_agent.py
+++ new_minigames/SentryForceField/scripted_agent.py
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# Thanks to : jmathison
+
 """Scripted agents."""
 
 from __future__ import absolute_import
@@ -46,6 +49,26 @@ _HAL_ZEALOT = actions.FUNCTIONS.Hallucination_Zealot_quick.id
 _FORCE_FIELD = actions.FUNCTIONS.Effect_ForceField_screen.id
 _GUARD_FIELD = actions.FUNCTIONS.Effect_GuardianShield_quick.id
 
+Hallucinations = [_HAL_ADEPT, _HAL_ARCHON, _HAL_DISRUP, _HAL_HIGTEM, _HAL_IMN, _HAL_PHOENIX, _HAL_STALKER, _HAL_VOIDRAID, _HAL_ZEALOT]
+
+class Hallucination(base_agent.BaseAgent):
+  """An agent specifically for solving the ForceField map."""
+
+  def step(self, obs):
+    super(Hallucination, self).step(obs)
+    if Hallucinations= in obs.observation["available_actions"]:
+      player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
+      hellion_y, hellion_x = (player_relative == _PLAYER_HOSTILE).nonzero()
+      if not hydralisk_y.any():
+        return actions.FunctionCall(_NO_OP, [])
+      index = numpy.argmax(hydralisk_y)
+      target = [hydralisk_x[index], hydralisk_y[index]]
+      return actions.FunctionCall(Hallucinations(np.random.randint(0,len(Hallucinations)), [_NOT_QUEUED])
+    elif _SELECT_ARMY in obs.observation["available_actions"]:
+      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
+    else:
+      return actions.FunctionCall(_NO_OP, [])
+                                  
 class SentryForceField(base_agent.BaseAgent):
   """An agent specifically for solving the ForceField map."""
 
