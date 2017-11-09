1c5eecb89f611390a6c159fe2c85e096e96b4bac
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-11-09 08:43:12 -0800

    Update q_learning agent.py

--- Agents/q_learning_agent.py
+++ Agents/q_learning_agent.py
@@ -1,6 +1,6 @@
 # Q-learning agent for HallucinIce. 
 # The goal for the agent is to discover wich combination of hallucination will be better to defeat 
-# Kudos to MorvanZhou , Steven Brow
+# Kudos to MorvanZhou , Steven Brown
 
 import random
 import math 
@@ -22,6 +22,34 @@ _ATTACK_SCREEN = actions.FUNCTIONS.Attack_screen.id
 _SELECT_ARMY = actions.FUNCTIONS.select_army.id
 _NOT_QUEUED = [0]
 _SELECT_ALL = [0]
+
+# Define the actions 
+ACTION_HAL_ADEPT = 'adept'
+ACTION_HAL_ARCHON = 'archon'
+ACTION_HAL_COL = 'colosus'
+ACTION_HAL_DISRUP = 'disruptor'
+ACTION_HAL_HIGTEM = 'higtem'
+ACTION_HAL_IMN = 'imortal'
+ACTION_HAL_PHOENIX = 'phoenix'
+ACTION_HAL_STALKER = 'stalker'
+ACTION_HAL_VOIDRAID = 'voidraid'
+ACTION_HAL_ZEALOT = 'zealot'
+
+Smart_actions = [
+  ACTION_NO_OP,
+  ACTION_HAL_ADEPT,
+  ACTION_HAL_ARCHON,
+  ACTION_HAL_COL,
+  ACTION_HAL_DISRUP,
+  ACTION_HAL_HIGTEM,
+  ACTION_HAL_IMN,
+  ACTION_HAL_PHOENIX,
+  ACTION_HAL_STALKER,
+  ACTION_HAL_VOIDRAID,
+  ACTION_HAL_ZEALOT 
+]
+
+KILL_UNIT_REWARD = 0.5
 
 
 class QLearnigTable:
@@ -69,30 +97,37 @@ class QLearnigTable:
         self.q_table = self.q_table.append(pd.Series([0] * len(self.actions), index=self.q_table.columns, name=state))
       
   
-# Define the actions 
-_HAL_ADEPT = 'adept'
-_HAL_ARCHON = 'archon'
-_HAL_COL = 'colosus'
-_HAL_DISRUP = 'disruptor'
-_HAL_HIGTEM = 'higtem'
-_HAL_IMN = 'imortal'
-_HAL_PHOENIX = 'phoenix'
-_HAL_STALKER = 'stalker'
-_HAL_VOIDRAID = 'voidraid'
-_HAL_ZEALOT = 'zealot'
-
-#Smart actions = [
-  _HAL_ADEPT,
-  _HAL_ARCHON,
-  _HAL_COL,
-  _HAL_DISRUP,
-  _HAL_HIGTEM,
-  _HAL_IMN,
-  _HAL_PHOENIX,
-  _HAL_STALKER,
-  _HAL_VOIDRAID,
-  _HAL_ZEALOT 
-]
-
-
+class SmartAgent(base_agent.BaseAgent):
+  def __init__(self):
+    self.qlearn = QLearningTable(actions=list(range(len(smart_actions))))
+    
+    self.previous_killed_unit_score = 0
+    self.previous_action = None
+    self.previous_state = None 
+    
+    #Compare step from scripted agent with learning agent 
+  def step(self, obs):
+    #-----------------#
+    #---#
+    #---#
+    
+    if self.previous_action is not None:
+      reward = 0
+      
+      if killed_unit_score > self.previous_killed_score:
+        reward += KILL_UNIT_REWARD
+        
+      self.qlearn.learn(str(self.previous_state), self.previous_action, reward, str(current_state))
+    
+    rl_action = self.qlearn.choose_action(str(current_state))
+    smart_action = smart_actions[rl_action]
+    
+    self.previous_killed_unit_score = killed_unit_score
+    self.previous_state = current_state
+    self.previous_action = rl_action
+    
+    if smart_action == ACTION_DO_NOTHING:
+      return actions.FunctionCall(_NO_OP, [])
+    
+    elif smart_action == ACTION_HAL_ARCHON:
 
